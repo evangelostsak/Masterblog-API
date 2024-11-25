ac24e574305623025c78fff2e0e2e7b66f0cc038
evangelostsak <baggtsak55@gmail.com>
2024-11-25 03:38:18 +0100

    Add Endpoint added

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -9,10 +9,49 @@ POSTS = [
     {"id": 2, "title": "Second post", "content": "This is the second post."},
 ]
 
+id_stat = len(POSTS) + 1
+
+
+def new_id_gen():
+    """Generates a unique id using our global id counter 'id_stat' """
+    global id_stat
+    new_id = id_stat
+    id_stat += 1
+    return new_id
+
 
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
+    """Post listing"""
     return jsonify(POSTS)
+
+
+@app.route('/api/posts', methods=['POST'])
+def add_post():
+    """Adding a post method"""
+    data = request.get_json()
+
+    # Error handling part for title and content + strip for cleanup
+    if not data:
+        return jsonify({"error": "Request must be json"}), 400
+
+    title = data.get('title', '').strip()
+    content = data.get('content', '').strip()
+
+    if not title:
+        return jsonify({"error": "Title is a required field"}), 400
+    if not content:
+        return jsonify({"error": "Content is a required field"}), 400
+
+    # ID generation process
+    new_post = {
+        "id": new_id_gen(),
+        "title": title,
+        "content": content
+    }
+    POSTS.append(new_post)
+
+    return jsonify(new_post), 201
 
 
 if __name__ == '__main__':
